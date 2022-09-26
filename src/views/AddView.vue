0b0d4396f0d2cbbcfe680788e6fd9c1bf65170b7
Manuel Balanta <mbalantaq@gmail.com>
2022-09-25 21:39:06 -0500

    Add view

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -2,11 +2,18 @@
     <header>
         <Header/>
     </header>
+    <main>
+        <Form/>
+    </main>
+    <footer>
+
+    </footer>
 </template>
 <script>
 import Header from "../components/Header.vue";
+import Form from "../components/Form.vue";
 export default{
-    components: { Header }
+    components: { Header, Form }
 }
 </script>
 
