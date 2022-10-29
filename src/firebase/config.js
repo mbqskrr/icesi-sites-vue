1721ac7359c26b2abf299b9cd14971cb4f9db3e0
Manuel Balanta <mbalantaq@gmail.com>
2022-10-29 00:19:37 -0500

    Getting started with firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmpuYpvM3BWgem4i7zRmHcodZBKaNt1sU",
+  authDomain: "icesi-sites.firebaseapp.com",
+  projectId: "icesi-sites",
+  storageBucket: "icesi-sites.appspot.com",
+  messagingSenderId: "331325292073",
+  appId: "1:331325292073:web:7b616062302a9a82222638",
+  measurementId: "G-L4R1B1CVGY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
